3ece3145a46f99d20fa8c1ad4d3e55c08617ecdf
Igor Chaves <ds.igorchaves@gmail.com>
2024-06-05 12:30:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from metodos.metodo_ponto_fixo import MetodoPontoFixo
-
+from metodos.ponto_fixo      import MetodoPontoFixo
+from metodos.newton_rhapson  import MetodoNewtonRhapson
+from metodos.secante         import MetodoSecante
 def main():
-    metodo = MetodoPontoFixo(c=1, q0=0.5)
+    # metodo = MetodoPontoFixo(c=1, q0=0.5)
+    # metodo.executar()
 
+    # metodo = MetodoNewtonRhapson(c=1, q0=0.5)
+    # metodo.executar()
+
+    metodo = MetodoSecante(c=1, q0=0.4, q1=0.8)
     metodo.executar()
 
 if __name__ == "__main__":
