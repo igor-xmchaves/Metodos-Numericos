83ccc6c19c77043fce1fdcf09d15c8bfd6a63e65
Igor Chaves <ds.igorchaves@gmail.com>
2024-07-02 22:13:07 -0300

    Update app.py

--- user/app.py
+++ user/app.py
@@ -1,9 +1,10 @@
 from tkinter                 import *
 from tkinter                 import ttk
+from timeit                  import *
 from metodos.ponto_fixo      import MetodoPontoFixo
 from metodos.newton_rhapson  import MetodoNewtonRhapson
 from metodos.secante         import MetodoSecante
-from timeit                    import *
+
 
 COLOR_BG_1 = '#FF7F50'
 COLOR_BG_2 = '#FFEBE2'
@@ -12,7 +13,7 @@ COLOR_BG_4 = 'BLACK'
 
 class Funcs(object):
     def style_treeview(self):
-        ### Estilização customizada da tabela1
+        ### Estilização customizada da tabela 1 e 2
         self.style = ttk.Style()
         self.style.theme_use("default")
         self.style.configure("Custom.Treeview", 
@@ -26,14 +27,20 @@ class Funcs(object):
                              font=("Arial", 9, 'bold'))
     
     def menu_submit(self):
-        
+        """
+        Função que muda as colunas da tabela 1 e as entradas de variáveis,
+        conforme o método selecionado pelo botão Submit
+        """
         self.style_treeview()
         
         ### Condição para mudar as variáveis e a tabela1 conforme o método
         if self.metodo_var.get() == "Ponto Fixo" or self.metodo_var.get() == "Newton-Rhapson":
+            
+            ### Esconde a label e a entrada de q1 e coloca a de Tol no lugar
             self.hide_label()
             self.hide_entry()
             
+            ### Criação da Tabela1
             self.tabela1 = ttk.Treeview(self.frame_2, height= 3, columns= ("col1", "col2", "col3", "col4", "col5"), style= "Custom.Treeview" )
             self.tabela1.heading("#0", text="")
             self.tabela1.heading("col1", text="Iter")
@@ -51,9 +58,7 @@ class Funcs(object):
             
             self.tabela1.place(relx=0.01, rely=0.01, relwidth=0.95, relheight=0.77)
 
-            """
-            Criação da tabela2
-            """
+            ### Criação da Tabela2
             self.tabela2 = ttk.Treeview(self.frame_2, height= 3, columns= ("col1", "col2"), style= "Custom.Treeview")
 
             self.tabela2.heading("#0", text="")
@@ -73,9 +78,11 @@ class Funcs(object):
     
 
         elif self.metodo_var.get() == "Secante":
+            ### Coloca a label e a entrada de q1 e muda de posição as de Tol 
             self.show_label()
             self.show_entry()
 
+            ### Criação	da Tabela1
             self.tabela1 = ttk.Treeview(self.frame_2, height= 3, columns= ("col1", "col2", "col3", "col4", "col5", "col6"), style= "Custom.Treeview")
             self.tabela1.heading("#0", text="")
             self.tabela1.heading("col1", text="Iter")
@@ -95,9 +102,7 @@ class Funcs(object):
 
             self.tabela1.place(relx=0.01, rely=0.01, relwidth=0.95, relheight=0.77)
             
-            """
-            Criação da tabela2
-            """
+            ### Criação da Tabela2
             self.tabela2 = ttk.Treeview(self.frame_2, height= 3, columns= ("col1", "col2"), style= "Custom.Treeview")
 
             self.tabela2.heading("#0", text="")
@@ -110,12 +115,12 @@ class Funcs(object):
 
             self.tabela2.place(relx=0.01, rely=0.80, relwidth=0.95, relheight=0.10)
 
-            ### Criação da scrollbar
+            ### Scrollbar
             self.scrollTabela = ttk.Scrollbar(self.frame_2, orient= 'vertical')
             self.tabela1.configure(yscroll=self.scrollTabela.set)
             self.scrollTabela.place(relx= 0.96, rely= 0.01, relwidth= 0.04, relheight= 0.77)
         
-        
+    
     def hide_label(self):
             self.label_q1.place_forget()
             self.label_tol.place_forget()
@@ -144,7 +149,6 @@ class Funcs(object):
             self.tol_entry = Entry(self.frame_3, background=COLOR_BG_2, 
                               font= ('Arial', '10'), justify= CENTER)
             self.tol_entry.place(relx=0.15, rely=0.65, relwidth= 0.70, relheight=0.07)
-
             
     def clear_tabela1(self):
         for item in self.tabela1.get_children():
@@ -153,7 +157,6 @@ class Funcs(object):
         for item in self.tabela2.get_children():
             self.tabela2.delete(item)
         
-
 ### Funções de execução dos métodos
     def ponto_fixo(self, metodo):
         """
@@ -166,6 +169,7 @@ class Funcs(object):
             q_next = metodo.funcaoIteracao(metodo.c, metodo.q0)
             e = q_next - metodo.q0
 
+            ### printa na tabela1 os valores de cada iteração 
             iteracoes, q1, q, erro, f_q1 = [k, q_next, metodo.q0, e, metodo.funcaoObjetivo(metodo.c, q_next)]
             self.tabela1.insert("", "end", values=(f"{iteracoes}", f"{q1:.10f}", f"{q:.10f}", f"{erro:.10f}", f"{f_q1:.10f}"))
 
@@ -185,12 +189,12 @@ class Funcs(object):
         """
         if abs(metodo.funcaoObjetivo(metodo.c, metodo.q0)) < metodo.tol:
             return metodo.q0
-            
 
         for k in range(metodo.max_iter):
             q_next = metodo.funcaoIteracao(metodo.c, metodo.q0)
             e = q_next - metodo.q0
 
+            ### printa na tabela1 os valores de cada iteração 
             iteracoes, q1, q, erro, f_q1 = [k, q_next, metodo.q0, e, metodo.funcaoObjetivo(metodo.c, q_next)]
             self.tabela1.insert("", "end", values=(f"{iteracoes}", f"{q1:.10f}", f"{q:.10f}", f"{erro:.10f}", f"{f_q1:.10f}"))
 
@@ -222,6 +226,7 @@ class Funcs(object):
             q_next = metodo.funcaoIteracao(metodo.c, metodo.q0, metodo.q1)
             e = q_next - metodo.q1
 
+            ### printa na tabela1 os valores de cada iteração 
             iteracoes, q2, q1, q0, erro, f_q2 = [k, q_next, metodo.q1, metodo.q0, e, metodo.funcaoObjetivo(metodo.c, q_next)]
             self.tabela1.insert("", "end", values=(f"{iteracoes}", f"{q2:.10f}", f"{q1:.10f}", f"{q0:.10f}", f"{erro:.10f}", f"{f_q2:.10f}"))
 
@@ -237,7 +242,12 @@ class Funcs(object):
 
         raise ValueError("O método de ponto fixo não convergiu após o número máximo de iterações.")
     def executar_metodo(self):
-        
+        """
+        Executa o método de ponto fixo, Newton-Raphson ou Secante
+        dependendo do método escolhido pelo usuário.
+        """
+
+        ### Limpa as tabelas para os novos valores serem inseridos a seguir
         self.clear_tabela1()
         self.clear_tabela2()
 
@@ -263,14 +273,17 @@ class Funcs(object):
             if self.metodo_selecionado == "Ponto Fixo":
                 raiz = self.ponto_fixo(metodo)
                 tempo = timeit(str(raiz), globals=globals(), number=1)
-                self.tabela2.insert("", "end", values= (f"{raiz:.10f}", f"{tempo:.10f}"))
+                ### printa na tabela 2 a raiz e tempo de execução
+                self.tabela2.insert("", "end", values= (f"{raiz:.10f}", f"{tempo:.10f}")) 
             elif self.metodo_selecionado == "Newton-Rhapson":
                 raiz = self.newton_rhapson(metodo)
                 tempo = timeit(str(raiz), globals=globals(), number=1)
+                ### printa na tabela 2 a raiz e tempo de execução
                 self.tabela2.insert("", "end", values= (f"{raiz:.10f}", f"{tempo:.10f}"))
             elif self.metodo_selecionado == "Secante":
                 raiz = self.secante(metodo)
                 tempo = timeit(str(raiz), globals=globals(), number=1)
+                ### printa na tabela 2 a raiz e tempo de execução
                 self.tabela2.insert("", "end", values= (f"{raiz:.10f}", f"{tempo:.10f}"))
         
         except Exception as e:
@@ -292,6 +305,9 @@ class Application(Funcs):
         self.widgets_frame2()
         self.widgets_frame3()    
     def tela(self):
+        """
+        Configuração da tela principal
+        """
         self.master.title("Métodos Numéricos")
         self.master.geometry("720x480")
         self.master.configure(background= COLOR_BG_3)
@@ -299,12 +315,18 @@ class Application(Funcs):
         self.master.maxsize(width=900, height=700)
         self.master.minsize(width=400, height=300)
     def frames_da_tela(self):
+        """
+        Criação dos frames
+        """
+        ### Frame1
         self.frame_1 = Frame(self.master, background= COLOR_BG_3)
         self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.28, relheight=0.30)
 
+        ### Frame2
         self.frame_2 = Frame(self.master, background=COLOR_BG_3)
         self.frame_2.place(relx=0.32, rely=0.02, relwidth=0.66, relheight=0.96)
 
+        ### Frame3
         self.frame_3 = Frame(self.master, background= COLOR_BG_3)
         self.frame_3.place(relx=0.02, rely=0.22, relwidth=0.28, relheight=0.76)
 
@@ -334,6 +356,7 @@ class Application(Funcs):
                                     
         self.metodo_menu.place(relx=0.15, rely=0.04, relwidth=0.70, relheight=0.20)
 
+        ### Botão Submit
         self.bt_submit = Button(self.frame_1, text='Submit', background=COLOR_BG_2, 
                                 highlightbackground=COLOR_BG_1, highlightthickness=3, 
                                 border=1, activebackground=COLOR_BG_2,
@@ -341,10 +364,12 @@ class Application(Funcs):
         self.bt_submit.place(relx=0.30, rely=0.26, relwidth=0.40, relheight=0.2)
     def widgets_frame2(self):
         """
-        Cria a estrutura base da tabela1 que aparece antes de selecionar o método
+        Criação a estrutura base da tabela1 e tabela2 que aparecem na tela inicial
         """
+        ### Estilização das tabelas
         self.style_treeview()
 
+        ### Criação da Tabela1
         self.tabela1 = ttk.Treeview(self.frame_2, height= 3, columns= ("col1", "col2", "col3", "col4", "col5", "col6"), style= "Custom.Treeview")
         self.tabela1.heading("#0", text="")
         self.tabela1.heading("col1", text="")
@@ -364,9 +389,8 @@ class Application(Funcs):
 
         self.tabela1.place(relx=0.01, rely=0.01, relwidth=0.95, relheight=0.77)
 
-        """
-        Criação da tabela2
-        """
+        
+        ### Criação da Tabela2
         self.tabela2 = ttk.Treeview(self.frame_2, height= 3, columns= ("col1", "col2"), style= "Custom.Treeview")
 
         self.tabela2.heading("#0", text="")
@@ -386,8 +410,9 @@ class Application(Funcs):
 
     def widgets_frame3(self):
         """
-        Criação da label e entrada do código
+        Criação das labels e entradas do código
         """
+        ### Label C
         self.label_c = Label(self.frame_3, text='C: ', background=COLOR_BG_3, 
                              font= ('Arial', '13', 'bold'))
         self.label_c.place(relx=0.45, rely=0.01)
@@ -395,6 +420,7 @@ class Application(Funcs):
                              font= ('Arial', '10'), justify= CENTER)
         self.c_entry.place(relx=0.15, rely=0.08, relwidth= 0.70, relheight=0.07)
 
+        ### Label q0
         self.label_q0 = Label(self.frame_3, text='Q0: ', background=COLOR_BG_3, 
                               font= ('Arial', '13', 'bold'))
         self.label_q0.place(relx=0.43, rely=0.20)
@@ -402,6 +428,7 @@ class Application(Funcs):
                               font= ('Arial', '10'), justify= CENTER)
         self.q0_entry.place(relx=0.15, rely=0.27, relwidth= 0.70, relheight=0.07)
 
+        ### Label q1
         self.label_q1 = Label(self.frame_3, text='Q1: ', background=COLOR_BG_3, 
                               font= ('Arial', '13', 'bold'))
         self.label_q1.place(relx=0.43, rely=0.39)
@@ -409,6 +436,7 @@ class Application(Funcs):
                               font= ('Arial', '10'), justify= CENTER)
         self.q1_entry.place(relx=0.15, rely=0.46, relwidth= 0.70, relheight=0.07)
 
+        ### Label Tol (erro absoluto)
         self.label_tol = Label(self.frame_3, text='Tol: ', background=COLOR_BG_3, 
                               font= ('Arial', '13', 'bold'))
         self.label_tol.place(relx=0.43, rely=0.58)
@@ -417,9 +445,7 @@ class Application(Funcs):
         self.tol_entry.place(relx=0.15, rely=0.65, relwidth= 0.70, relheight=0.07)
 
 
-        """
-        Criação botão executar
-        """
+        ### Botão Executar
         self.bt_executar = Button(self.frame_3, text='Executar', background=COLOR_BG_2, 
                                   highlightbackground=COLOR_BG_3, highlightthickness=3,
                                   activebackground= COLOR_BG_2,
