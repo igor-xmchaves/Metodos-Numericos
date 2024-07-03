ae22bc2893c1e0510e6d3bfb3daa8f18b0ac0f68
Igor Chaves <ds.igorchaves@gmail.com>
2024-07-02 21:51:32 -0300

    Update secante.py

--- metodos/secante.py
+++ metodos/secante.py
@@ -1,7 +1,7 @@
 import math
 
 class MetodoSecante:
-    def __init__(self, c, q0, q1, tol=1e-4, max_iter=100):
+    def __init__(self, c, q0, q1, tol, max_iter=100):
         """
         Inicializa o método de ponto fixo.
         """
